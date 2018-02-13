ce57d7b541f8bcb651ff21221ee8a18490cfa47a
poustmal <poustmal12@gmail.com>
2018-02-13 13:47:02 +0200

    Upload

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -2449,6 +2449,8 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
                         if($cancelled){
                             $ev->setCancelled();
                         }
+						if(!$this->isSprinting() and !$this->isFlying() and $this->fallDistance > 0 and !$this->hasEffect(Effect::BLINDNESS) and !$this->isInsideOfWater()){
+							$ev->setDamage($ev->getFinalDamage() / 2, EntityDamageEvent::MODIFIER_CRITICAL);						}
 
                         $target->attack($ev);
 
@@ -2458,6 +2460,9 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
                             }
                             return true;
                         }
+						if($ev->getDamage(EntityDamageEvent::MODIFIER_CRITICAL) > 0){							$pk = new AnimatePacket();							$pk->action = AnimatePacket::ACTION_CRITICAL_HIT;
+							$pk->entityRuntimeId = $target->getId();
+							$this->server->broadcastPacket($target->getViewers(), $pk);							if($target instanceof Player){								$target->dataPacket($pk);							}						}
 
                         if($this->isSurvival()){
                             if($heldItem->useOn($target)){
