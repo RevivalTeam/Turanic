945b25ad9c724c33cfb45025a3b441011d12958b
poustmal <poustmal12@gmail.com>
2018-04-10 21:24:52 +0300

    Add files via upload

--- src/pocketmine/level/particle/TerrainParticle.php
+++ src/pocketmine/level/particle/TerrainParticle.php
@@ -19,19 +19,16 @@
  *
 */
 
+declare(strict_types=1);
+
 namespace pocketmine\level\particle;
 
 use pocketmine\block\Block;
+use pocketmine\block\BlockFactory;
 use pocketmine\math\Vector3;
 
-class TerrainParticle extends GenericParticle {
-	/**
-	 * TerrainParticle constructor.
-	 *
-	 * @param Vector3 $pos
-	 * @param Block   $b
-	 */
+class TerrainParticle extends GenericParticle{
 	public function __construct(Vector3 $pos, Block $b){
-		parent::__construct($pos, Particle::TYPE_TERRAIN, ($b->getDamage() << 8) | $b->getId());
+		parent::__construct($pos, Particle::TYPE_TERRAIN, BlockFactory::toStaticRuntimeId($b->getId(), $b->getDamage()));
 	}
 }
